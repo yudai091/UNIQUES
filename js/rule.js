88781e8f3a52a6f94e07bc045f0f9895672f0693
yudai091 <yudai5707@gmail.com>
2023-10-28 16:36:17 +0900

    プレイヤーネームの変更機能追加

--- js/rule.js
+++ js/rule.js
@@ -8,14 +8,6 @@ var page1 = document.getElementById("page1");
 var page2 = document.getElementById("page2");
 var page3 = document.getElementById("page3");
 var page4 = document.getElementById("page4");
-var close = document.getElementById("close");
-
-close.addEventListener("click", () => {
-  page1.style.display = "block";
-  page2.style.display = "none";
-  page3.style.display = "none";
-  page4.style.display = "none";
-});
 
 nextBtn1.addEventListener("click", () => {
   page1.style.display = "none";
--- js/setting.js
+++ js/setting.js
@@ -1,3 +1,4 @@
+//人数とカード枚数の初期値
 var playerNum = 4;
 var cardsKind = 12;
 var playerNumBtn = document.querySelectorAll(".radio-num__input");
@@ -9,6 +10,7 @@ for (let i = 0; i < playerNumBtn.length; i++) {
     window.localStorage.setItem("playersNum", playerNum);
   }
   playerNumBtn[i].addEventListener("change", () => {
+    numChangeTrigger = true;
     playerNum = document.querySelector(".radio-num__input:checked").value;
     window.localStorage.setItem("playersNum", playerNum);
   });
@@ -30,45 +32,52 @@ var numConf = document.getElementById("numConf");
 var nameChangeBox = document.getElementById("nameChangeBox");
 var nameList = document.getElementById("nameList");
 var playersName = [];
-var created = false;
+var numChangeTrigger = false;
 numConf.addEventListener("click", () => {
-  if (created) {
+  //人数変更時に発火
+  if (numChangeTrigger) {
     var children = document.querySelectorAll(".playerNameBox");
     for (let i = 0; i < children.length; i++) {
       nameList.removeChild(children[i]);
+      playersName.pop();
     }
-    created = false;
+    numChangeTrigger = false;
   }
-  if (!created) {
+  //名前変更inputの生成
+  if (!numChangeTrigger) {
     for (let i = 1; i <= playerNum; i++) {
       var nameBox = document.createElement("input");
       nameBox.className = "playerNameBox " + "playerName" + i;
-      nameBox.id = "player_" + i + "_" + playersName;
+      nameBox.id = "player_" + i;
       nameBox.placeholder = "player" + i;
       playersName.push(nameBox);
       nameList.appendChild(playersName[i - 1]);
     }
-    created = true;
   }
   nameChangeBox.style.opacity = "1";
   nameChangeBox.style.zIndex = "100";
   nameChangeBox.style.visibility = "visible";
 });
 var close = document.getElementById("close");
+var nameChangeMark = document.getElementById("nameChangeMark");
 close.addEventListener("click", () => {
+  var cnt = 0; //名前変更したことを教えるマークの表示設定
   nameChangeBox.style.opacity = "0";
   nameChangeBox.style.zIndex = "0";
   nameChangeBox.style.visibility = "hidden";
   for (let i = 1; i <= playerNum; i++) {
     if (playersName[i - 1].value) {
+      cnt++;
       var playerName = playersName[i - 1].value;
       window.localStorage.setItem("playerName" + i, playerName);
     } else {
       var playerName = playersName[i - 1].placeholder;
       window.localStorage.setItem("playerName" + i, playerName);
     }
-    console.log(
-      "P" + i + " : " + window.localStorage.getItem("playerName" + i)
-    );
+  }
+  if (cnt > 0) {
+    nameChangeMark.style.display = "block";
+  } else {
+    nameChangeMark.style.display = "none";
   }
 });
